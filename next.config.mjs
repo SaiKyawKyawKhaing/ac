4c93ab65cb751e9c882d621e44462cacb07c6583
kyawkyawkhaing@abank.com.mm <kyawkyawkhaing@abank.com.mm>
2025-02-10 21:46:01 +0630

    production

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.NEXT_PUBLIC_BASE_URL || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_URL || '',
+  reactStrictMode: true,
+  // Conditionally set basePath and assetPrefix for production (GitHub Pages)
+  basePath: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASE_URL: '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASE_URL : '',
 };
-
-module.exports = nextConfig;
-
 
 export default nextConfig;
